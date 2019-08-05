ea1337df630b31eaf6f6beacb9b15a8156976a79
Student <example@example.com>
2019-08-05 16:28:53 -0700

    add validation and callback functionality on animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,12 @@
 class Animal < ApplicationRecord
   has_many :sightings, dependent: :destroy
+  validates :name, presence: true
+  validates_length_of :name, maximum: 100
+
+  before_save(:titleize_album)
+
+private
+  def titleize_album
+    self.name = self.name.titleize
+  end
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 describe Animal do
   it {  should have_many (:sightings)  }
+  it {  should validate_presence_of :name  }
+  it {  should validate_length_of(:name).is_at_most(100)  }
+end
+
+describe Animal do
+  it("titleizes the name of an animal") do
+    animal = Animal.create({name: "giant steps"})
+    expect(animal.name()).to(eq("Giant Steps"))
+  end
 end
