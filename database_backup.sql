26bec4ebf606c77579ee92cd7073b8ec62971080
Student <example@example.com>
2019-08-05 16:31:35 -0700

    add backups

---
+++ database_backup.sql
@@ -0,0 +1,235 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.4
+-- Dumped by pg_dump version 11.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: animals; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.animals (
+    id bigint NOT NULL,
+    name character varying,
+    type character varying,
+    number integer,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.animals OWNER TO "Guest";
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.animals_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.animals_id_seq OWNER TO "Guest";
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.animals_id_seq OWNED BY public.animals.id;
+
+
+--
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.ar_internal_metadata (
+    key character varying NOT NULL,
+    value character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.ar_internal_metadata OWNER TO "Guest";
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.schema_migrations (
+    version character varying NOT NULL
+);
+
+
+ALTER TABLE public.schema_migrations OWNER TO "Guest";
+
+--
+-- Name: sightings; Type: TABLE; Schema: public; Owner: Guest
+--
+
+CREATE TABLE public.sightings (
+    id bigint NOT NULL,
+    description character varying,
+    lattitude integer,
+    animal_id integer,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    longitude integer,
+    region_type character varying
+);
+
+
+ALTER TABLE public.sightings OWNER TO "Guest";
+
+--
+-- Name: sightings_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE public.sightings_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.sightings_id_seq OWNER TO "Guest";
+
+--
+-- Name: sightings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE public.sightings_id_seq OWNED BY public.sightings.id;
+
+
+--
+-- Name: animals id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.animals ALTER COLUMN id SET DEFAULT nextval('public.animals_id_seq'::regclass);
+
+
+--
+-- Name: sightings id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.sightings ALTER COLUMN id SET DEFAULT nextval('public.sightings_id_seq'::regclass);
+
+
+--
+-- Data for Name: animals; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.animals (id, name, type, number, created_at, updated_at) FROM stdin;
+7	Tiger	\N	\N	2019-08-05 23:28:05.27101	2019-08-05 23:28:05.27101
+\.
+
+
+--
+-- Data for Name: ar_internal_metadata; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.ar_internal_metadata (key, value, created_at, updated_at) FROM stdin;
+environment	development	2019-08-05 18:26:33.368286	2019-08-05 18:26:33.368286
+\.
+
+
+--
+-- Data for Name: schema_migrations; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.schema_migrations (version) FROM stdin;
+20190805182351
+20190805182905
+20190805183416
+20190805213533
+20190805214340
+20190805230058
+\.
+
+
+--
+-- Data for Name: sightings; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY public.sightings (id, description, lattitude, animal_id, created_at, updated_at, longitude, region_type) FROM stdin;
+\.
+
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.animals_id_seq', 7, true);
+
+
+--
+-- Name: sightings_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('public.sightings_id_seq', 7, true);
+
+
+--
+-- Name: animals animals_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.animals
+    ADD CONSTRAINT animals_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.ar_internal_metadata
+    ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: sightings sightings_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.sightings
+    ADD CONSTRAINT sightings_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sightings fk_rails_37c051df87; Type: FK CONSTRAINT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY public.sightings
+    ADD CONSTRAINT fk_rails_37c051df87 FOREIGN KEY (animal_id) REFERENCES public.animals(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
